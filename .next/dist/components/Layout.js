be53ff59f3f9647e23617fd7f41e7bebcde44e85
Jason Kwong <jasonkwong11@gmail.com>
2018-02-24 22:11:25 -0500

    add form validation

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -14,6 +14,10 @@ var _Header = require('./Header.js');
 
 var _Header2 = _interopRequireDefault(_Header);
 
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/jasonkwong/developer/kickstarter-on-ethereum/components/Layout.js';
@@ -22,18 +26,30 @@ exports.default = function (props) {
   return _react2.default.createElement(_semanticUiReact.Container, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
-    }
-  }, _react2.default.createElement('div', {
-    __source: {
-      fileName: _jsxFileName,
       lineNumber: 8
     }
-  }, _react2.default.createElement(_Header2.default, {
+  }, _react2.default.createElement(_head2.default, {
     __source: {
       fileName: _jsxFileName,
       lineNumber: 9
     }
+  }, _react2.default.createElement('link', {
+    rel: 'stylesheet',
+    href: '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css',
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    }
+  })), _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    }
+  }, _react2.default.createElement(_Header2.default, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    }
   }), props.children));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVM7O0FBQ1QsQUFBTyxBQUFZLEFBRW5COzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN4Qjt5QkFDRSxBQUFDOztnQkFBRDtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBLEFBQ0U7QUFERjtBQUFBLHFCQUNFLEFBQUM7O2dCQUFEO2tCQURGLEFBQ0UsQUFDRztBQURIO0FBQUEsWUFITixBQUNFLEFBQ0UsQUFFVyxBQUloQjtBQVREIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvamFzb25rd29uZy9kZXZlbG9wZXIva2lja3N0YXJ0ZXItb24tZXRoZXJldW0ifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwiSGVhZCIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUNULEFBQU8sQUFBWTs7OztBQUNuQixBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFVBQUEsQUFBQyxPQUFVLEFBQ3hCO3lCQUNFLEFBQUM7O2dCQUFEO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsQUFBQzs7Z0JBQUQ7a0JBQUEsQUFDRTtBQURGO0FBQUE7U0FDRSxBQUNNLEFBQ0o7VUFGRixBQUVPOztnQkFGUDtrQkFGSixBQUNFLEFBQ0UsQUFLRjtBQUxFO0FBQ0UsdUJBSUosY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEscUJBQ0UsQUFBQzs7Z0JBQUQ7a0JBREYsQUFDRSxBQUNHO0FBREg7QUFBQSxZQVROLEFBQ0UsQUFPRSxBQUVXLEFBSWhCO0FBZkQiLCJmaWxlIjoiTGF5b3V0LmpzIiwic291cmNlUm9vdCI6Ii9Vc2Vycy9qYXNvbmt3b25nL2RldmVsb3Blci9raWNrc3RhcnRlci1vbi1ldGhlcmV1bSJ9
--- pages/show.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return <h1>Welcome ot the show page!</h1>;
-};
